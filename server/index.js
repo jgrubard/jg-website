c82572bdd377814a5a796e6f84d5d1e3f27f696d
Jeremy Grubard <jgrubard@gmail.com>
2018-09-18 18:29:23 -0400

    fixed port

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = 3000 || process.env.PORT;
+const port = process.env.PORT || 3000;
 require('dotenv').config();
 
 app.use(require('body-parser').json());
